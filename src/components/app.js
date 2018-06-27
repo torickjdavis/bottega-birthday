79698b0367d888227b9fd4c705255e2fd0b42037
Torick Davis <torickjdavis@gmail.com>
2018-06-27 23:08:21 +0000

    Functional Button Component and Styles

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Picker from './picker';
+import Button from './button';
 
 export default class App extends Component {
   render() {
@@ -9,6 +10,7 @@ export default class App extends Component {
         <div className="skew-dark"></div>
         <div className="skew-light"></div>
         <Picker />
+        { Button(`Generate Countdown`) }
       </div>
     );
   }
