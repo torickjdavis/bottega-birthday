ca3a6d7e2083a0f218cce1f41241d1e944482521
Torick Davis <torickjdavis@gmail.com>
2018-06-28 06:07:41 +0000

    Added LargeText and remaining label. Also, finished front-end CSS.

--- src/components/app.js
+++ src/components/app.js
@@ -3,6 +3,7 @@ import Picker from './picker';
 import Button from './button';
 import Clock from './clock';
 import ChangeDate from './changeDate';
+import LargeText from './largeText';
 
 export default class App extends Component {
   constructor(props) {
@@ -17,11 +18,16 @@ export default class App extends Component {
     if (this.state.active) {
       return [
         <Clock />,
-        ChangeDate('Change Date', () => this.setState({ active: false }))
+        ChangeDate('Change Date', () => this.setState({ active: false })),
+        LargeText('04/03'),
+        <label className="grid__remaining">Remaining until your num-th birthday!</label>
       ];
     }
     else {
-      return Button('Generate Countdown', () => this.setState({ active: true }));
+      return [
+        <Picker />,
+        Button('Generate Countdown', () => this.setState({ active: true }))
+      ];
     }
   }.bind(this);
 
@@ -32,7 +38,6 @@ export default class App extends Component {
         <h1>Birthday Countdown</h1>
         <div className="skew-dark"></div>
         <div className="skew-light"></div>
-        <Picker />
         { this.renderItems() }
       </div>
     );
---
+++ src/components/largeText.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const largeText = (title) => {
+	return (
+		<div className="largeText">
+			{ title }
+		</div>
+	);
+};
+
+export default largeText;
