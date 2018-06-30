18d510b9db20601fb0e12b05eccac3999c0a8f9f
Torick Davis <torickjdavis@gmail.com>
2018-06-30 19:11:36 +0000

    Created handleChange for date information. Started the functionality.

--- src/components/app.js
+++ src/components/app.js
@@ -5,14 +5,43 @@ import Clock from './clock';
 import ChangeDate from './changeDate';
 import LargeText from './largeText';
 
+import moment from 'moment';
+
 export default class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      active: false
+      active: false,
+      startDate: moment()
     };
   }
+
+  handleGenerate = function() {
+    this.setState({ active: true });
+
+    var countDowndate = new Date("Sep 5, 2018 15:37:25").getTime();
+
+    var x = setInterval(() => {
+      //get today
+
+      var now = new Date().getTime();
+      var distance = countDowndate - now;
+
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor(distance % (1000 * 60 * 60 * 24)) / (1000 * 60);
+      var minutes = Math.floor(distance % (1000 * 60 * 60)) / (1000 * 60);
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      const time = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+      console.log(time);
+
+      if (distance < 0) { // if the countdown is over, stop
+        clearInterval(x);
+      }
+    }, 1000); // 1000ms (aka 1s)
+  }.bind(this);
+
 
   renderItems = function() {
     if (this.state.active) {
@@ -26,7 +55,7 @@ export default class App extends Component {
     else {
       return [
         <Picker />,
-        Button('Generate Countdown', () => this.setState({ active: true }))
+        Button('Generate Countdown', () => this.handleGenerate())
       ];
     }
   }.bind(this);
