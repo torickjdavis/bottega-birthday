f4dfa9eba83c67345984c96fc12576938c1ce979
Torick Davis <torickjdavis@gmail.com>
2018-06-30 23:29:26 +0000

    Added new Button class to changeDate component button.

--- src/components/changeDate.js
+++ src/components/changeDate.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ChangeDate = (title, callback) => {
 	return (
-		<button key={1} className="changeDate" onClick={callback}>
+		<button key={1} className="button changeDate" onClick={callback}>
 			<i className="far fa-calendar-alt"></i>
 			{ title }
 		</button>
