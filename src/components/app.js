7c21b864f1986d888b63f915df553ed82c2c6b47
Torick Davis <torickjdavis@gmail.com>
2018-06-30 19:14:28 +0000

    Corrected countdown calculation error.

--- src/components/app.js
+++ src/components/app.js
@@ -29,8 +29,8 @@ export default class App extends Component {
       var distance = countDowndate - now;
 
       var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      var hours = Math.floor(distance % (1000 * 60 * 60 * 24)) / (1000 * 60);
-      var minutes = Math.floor(distance % (1000 * 60 * 60)) / (1000 * 60);
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60));
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
       var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
       const time = `${days}d ${hours}h ${minutes}m ${seconds}s`;
